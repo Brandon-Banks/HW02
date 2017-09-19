9dcca32d64f3faf4ef8dca947d1820af6589b71f
Brandon Banks <brandonjb907@outlook.com>
2017-09-18 21:08:44 -0700

    Update Array_Based_PD.h

--- PhoneDirectory/Array_Based_PD.h
+++ PhoneDirectory/Array_Based_PD.h
@@ -1,6 +1,8 @@
 #ifndef ARRAY_BASED_PD_H_
 #define ARRAY_BASED_PD_H_
 #include <string>
+using namespace std;
+
 
 /** Specification file for the array-based phone directory.
  */
@@ -20,7 +22,7 @@ public:
 		@param source_name The name of the file (data source)
 		with the phone directory entries.
 		*/
-	void load_data(const std::string& source_name);
+	void load_data(const string& source_name);
 
 	/** Look up an entry.
 		@param name The name of the person to look up
@@ -28,7 +30,7 @@ public:
 		or an empty string if name is not in
 		the directory
 		*/
-	std::string lookup_entry(const std::string& name) const;
+	string lookup_entry(const string & name) const;
 
 	/** Changes the number associated with the given name to
 		the new value, or adds a new entry with this name and
@@ -38,15 +40,15 @@ public:
 		@return The old value of number or an empty string
 		if this is a new entry
 		*/
-	std::string add_or_change_entry(const std::string& name,
-		const std::string& number);
+	string add_or_change_entry(const string& name,
+		const string& number);
 
 	/** Removes the entry with the specified name from the directory.
 		@param name The name of the person
 		@return The person's name or an empty string if not in
 		the directory
 		*/
-	std::string remove_entry(const std::string& name);
+	string remove_entry(const string& name);
 
 	/** Writes the contents of the directory to the data file.
 	 */
@@ -54,21 +56,30 @@ public:
 
 	class Directory_Entry
 	{
-	public:
-		Directory_Entry() {} // Default no-argument constructor 
-		Directory_Entry(std::string the_name, std::string the_number) {
-			// constructor not implemented yet
+	private: // Lindsey and Brandon both worked on this together in the lab
+		string name;
+		string number;
+
+	public: // Lindsey and Brandon both worked on this together in the lab
+		Directory_Entry() {} 
+		Directory_Entry(string the_name, string the_number) 
+		{
+			name = the_name;
+			number = the_number;
 		}
-		std::string get_name() const {
-			return ""; // method not implemented yet
+		string get_name() const 
+		{
+			return name; 
 		}
-		std::string get_number() const {
-			return ""; // method not implemented yet
+		string get_number() const 
+		{
+			return number; 
 		}
-		void set_number(const std::string& new_number) {
-			// method not implemented yet
+		void set_number(const string& new_number)
+		{
+			number = new_number;
 		}
-	private:
+	
 
 	};
 
